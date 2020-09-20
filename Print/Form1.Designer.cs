d1d96b65bcbd59f8be868500f2dd78043a1ded56
Matthew Foster <Matthew.Foster@Civica.co.uk>
2020-09-20 11:36:08 +0100

    Update button test

--- Print/Form1.Designer.cs
+++ Print/Form1.Designer.cs
@@ -37,17 +37,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(217, 359);
+            this.button1.Location = new System.Drawing.Point(51, 207);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(179, 54);
+            this.button1.Size = new System.Drawing.Size(369, 54);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Print Text";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(135, 84);
+            this.textBox1.Location = new System.Drawing.Point(51, 28);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(369, 173);
@@ -72,7 +72,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 542);
+            this.ClientSize = new System.Drawing.Size(469, 307);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
